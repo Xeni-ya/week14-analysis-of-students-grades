c17083650a987ba64561caec154cb11d278a91e5
Xeni-ya <budyuginaxeniya@gmail.com>
2024-09-20 00:22:11 +0300

    fix: fixed the output to the screen

--- assets/script/script.js
+++ assets/script/script.js
@@ -3,7 +3,8 @@
 // Шаги выполнения:
 
 // Создайте пустой массив с именем grades и заполните массив случайными оценками от 1 до 100 для 12 студентов. Для генерации случайных оценок, используйте функцию Math.random().
-const list = document.querySelector('.one-result')
+const elem = document.querySelector('.results');
+
 function getRandomInt(min, max) {
 	return Math.floor(Math.random() * (max - min + 1)) + min;
 }
@@ -13,18 +14,12 @@ for (let i = 0; i < 12; i++) {
 	grades.push(getRandomInt(1, 100));
 }
 
-//console.log(`Оценки студентов: ${grades}`);
-let p = document.createElement('p');
-p.textContent = `Оценки студентов: ${grades}`;
-list.append(p);
-
 // Рассчитайте и выведите средний балл студентов, используя методы массивов.
 let sum = grades.reduce((sum, grades) => {
   return sum + grades;
 });
 
 let averageStudentScore = sum / grades.length;
-console.log(`Средний балл: ${averageStudentScore.toFixed(2)}`); 
 
 // Найдите и выведите максимальный балл среди студентов, используя соответствующий метод массива.
 
@@ -32,23 +27,19 @@ console.log(`Средний балл: ${averageStudentScore.toFixed(2)}`);
 //   return Math.max.apply(null, numArray);
 // }
 let maxScore = Math.max(...grades);
-console.log(`Максимальный балл: ${maxScore}`);
 
 // Найдите и выведите минимальный балл среди студентов, используя соответствующий метод массива.
 let minScore = Math.min(...grades);
-console.log(`Минимальный балл: ${minScore}`);
 
 // Посчитайте и выведите количество студентов, получивших положительную оценку (балл выше или равен 60). Используйте метод массива, который фильтрует оценки и оставляет только те, которые соответствуют заданному условию. Затем посчитайте количество оставшихся оценок и выведите результат.
 let numPositiveGrades = grades.filter(function (grade) {
   return grade >= 60;
 });
-console.log(`Количество студентов, получивших положительную оценку:  ${numPositiveGrades.length}`);
 
 // Посчитайте и выведите количество студентов, получивших отрицательную оценку (балл ниже 60). Используйте аналогичный подход, как в предыдущем шаге.
 let numNegativeGrades = grades.filter(function (grade) {
   return grade <= 60;
 });
-console.log(`Количество студентов, получивших отрицательную оценку: ${numNegativeGrades.length}`);
 
 // Преобразуйте числовые оценки в формат буквенных оценок A/B/C/D/E, используя следующие правила:
 // Если оценка находится в диапазоне от 80 до 100, преобразуйте её в "A"
@@ -70,5 +61,30 @@ let letterGrades = grades.map((grade) => {
 console.log(`Оценки в буквенном формате: ${letterGrades}`);
 
 // Выведите все найденные значения на экран.
-let resultsDiv = document.querySelector('.results');
-resultsDiv.innerHTML = `Оценки студентов: ${grades.join(', ')}`;
+let oneResult = document.createElement('p');
+oneResult.textContent = `Оценки студентов: ${grades}`;
+elem.append(oneResult);
+
+let towResult = document.createElement('p');
+towResult.textContent = `Средний балл: ${averageStudentScore.toFixed(2)}`;
+elem.append(towResult);
+
+let threeResult = document.createElement('p');
+threeResult.textContent = `Максимальный балл: ${maxScore}`;
+elem.append(threeResult);
+
+let fourResult = document.createElement('p');
+fourResult.textContent = `Минимальный балл: ${minScore}`;
+elem.append(fourResult);
+
+let fiveResult = document.createElement('p');
+fiveResult.textContent = `Количество студентов, получивших положительную оценку:  ${numPositiveGrades.length}`;
+elem.append(fiveResult);
+
+let sixResult = document.createElement('p');
+sixResult.textContent = `Количество студентов, получивших отрицательную оценку: ${numNegativeGrades.length}`;
+elem.append(sixResult);
+
+let sevenResult = document.createElement('p');
+sevenResult.textContent = `Оценки в буквенном формате: ${letterGrades}`;
+elem.append(sevenResult);
